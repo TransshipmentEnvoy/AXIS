c19a43edc097ccf3ca3aa50511be0fd5083dc839
EmperorJake <68182631+EmperorJake@users.noreply.github.com>
2026-02-03 21:53:32 +1100

    fix furniture factory acceptance bug

--- src/industries/furniture_factory.py
+++ src/industries/furniture_factory.py
@@ -19,7 +19,7 @@ industry.economy_variations["BASIC_TROPIC"].enabled = True
 industry.economy_variations["BASIC_TROPIC"].accept_cargos_with_input_ratios = [
     ("WDPR", 4),
     ("TEXT", 2),
-    ("STSE", 2),
+    ("MPAR", 2),
     ("GLAS", 2),
     ("COAT", 2),
 ]
@@ -32,7 +32,7 @@ industry.economy_variations["BASIC_TEMPERATE"].enabled = True
 industry.economy_variations["BASIC_TEMPERATE"].accept_cargos_with_input_ratios = [
     ("WDPR", 4),
     ("TEXT", 2),
-    ("STSE", 2),
+    ("MPAR", 2),
     ("GLAS", 2),
 ]
 industry.economy_variations["BASIC_TEMPERATE"].prod_cargo_types_with_output_ratios = [
